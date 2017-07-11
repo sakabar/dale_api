1149018984a22ff6969212331e06f89b16268330
Takafumi Sakakibara <tsakaki@lr.pi.titech.ac.jp>
2017-07-11 21:58:23 +0900

    New: setup maven

---
+++ src/main/java/info/saxcy/dale_api/DaleApiApplication.java
@@ -0,0 +1,12 @@
+package info.saxcy.dale_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaleApiApplication.class, args);
+	}
+}
